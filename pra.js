988f5f7aeb2be0b5d5aa2d6c4c536ddecae0fecc
sant0034 <87804487+sant0034@users.noreply.github.com>
2025-07-02 07:57:18 +0000

    update

---
+++ pra.js
@@ -0,0 +1,24 @@
+let x,y,z;
+x=9;y=6;z=x+y; console.log('z');
+//alert(3+5)
+//write(3+2);
+let a;
+a=122;
+console.log(a);
+
+let color = "Green";
+switch (color){
+    case "red":
+        console.log("Stop");
+        break;
+    case "yellow":
+        console.log("Go Slow");
+        break;
+    case "green":
+        console.log("Go");
+        break;
+    default:
+        console.log("Light is broken");
+}
+let  d="John"+" "+"Doe";
+console.log(d);
