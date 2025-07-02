8f74294239cd2b9cefa3bd35839af0a353c903e9
sant0034 <87804487+sant0034@users.noreply.github.com>
2025-07-02 11:59:18 +0000

    conversion

---
+++ 01_basics/opt.js
@@ -0,0 +1,39 @@
+let score = "Pritam";   
+//console.log(typeof score);
+//console.log(typeof(score));
+
+// So lets convert the type 
+let valueInNumber = Number(score); // here we convert it ,but this is not actually convert value
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);  // see the value and in null score it give 0,undefined gives NaN
+// and for boolean value it gives 1 for true and 0 for false.
+
+
+
+
+// Lets convert the boolen value
+
+let isLoggedIn = "";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+//console.log(typeof booleanIsLoggedIn);
+
+//console.log(booleanIsLoggedIn);
+
+// so when we pass "" result is false and when we pass "Pritam" result is true.
+
+//  now lets convert number it in to string
+
+let someNumber = 34;
+
+let stringNumber = String(someNumber);
+
+console.log(typeof stringNumber); // So here it convert internally also.
+
+console.log(stringNumber);
+
+
+
+
+
