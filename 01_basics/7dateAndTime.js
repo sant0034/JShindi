d76447cca1d5f1e62ad8fc9dcb8c7f3aca533595
sant0034 <87804487+sant0034@users.noreply.github.com>
2025-07-04 10:11:37 +0000

    update

---
+++ 01_basics/7dateAndTime.js
@@ -0,0 +1,18 @@
+// ++++++++++++++++++  Dates  +++++++++++++++++++
+let myDate = new Date()
+
+console.log(typeof myDate);
+
+console.log(myDate);
+
+console.log(myDate.toString());
+
+console.log(myDate.toDateString());
+
+console.log(myDate.toLocaleString());
+
+console.log(myDate.toLocaleDateString());
+
+
+
+
