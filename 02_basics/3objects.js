3795e22185b4a6e59911baeadf3424ce3ed88a6c
sant0034 <87804487+sant0034@users.noreply.github.com>
2025-07-08 10:01:22 +0000

    object

--- 02_basics/3objects.js
+++ 02_basics/3objects.js
@@ -21,20 +21,55 @@ const JsUser = {
     lastLoginDays: ["Monday" ,"Saturday"]
 }
 // how to access the data of object
-console.log(JsUser.name); // but this is not full way but it does not me it's wrong way.And you can't access the full name value 
+//console.log(JsUser.name); // but this is not full way but it does not me it's wrong way.And you can't access the full name value 
 //console.log(JsUser.full name);
 
-console.log(JsUser["full name"]); // by this you can access full name value and all other's value also.
+//console.log(JsUser["full name"]); // by this you can access full name value and all other's value also.
+
+
+
+
 
 // create a symbole which is primitive data type add into object and print them
-
 const mySym = Symbol("key1")
 const dataT = {
     name: "Prit",
-    mySym: "I am the symbole"
+    [mySym]: "I am the symbole"
 }
 
-console.log(dataT["name"]);
-console.log(typeof mySym);  // i create data type in object but in output it show data type is symbole.
+// console.log(dataT["name"]);
+// console.log(dataT[mySym]);
+// console.log(typeof dataT[mySym]);  
+//console.log(Object.freeze(dataT));  // here it that he is symbol not a string
 
+
+// So now Change the in object.
+//JsUser.email = "sant@google.com"  // here override the value
+//console.log(JsUser["email"]);
+
+// if you want to freez the JsUser so that no one change the value.
+// console.log(Object.freeze(JsUser));
+
+//JsUser.location = "Varanasi"  // here you can't override the value. because it's freez
+// console.log(JsUser["location"]);
+
+
+
+
+
+// how to add function in object. it treet them as variable.
+JsUser.greeting = function(){
+    console.log("Hello I am newly added function in object");
+    
+}
+// obejct JsUser ke pass kon kon si property hai use access lene ke liye use karte hai
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user , ${this.name}`);
+    
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
+
+console.log(JsUser);
 
