a2d3259f99a4d46724936ff77a2f9f24d9a7625b
sant0034 <87804487+sant0034@users.noreply.github.com>
2025-07-13 06:54:59 +0000

    update

--- 03_basics/01_functions.js
+++
@@ -1,1 +0,0 @@
-// function package of code ek baar create kar do ab use jaha le jana hai uski copy le jao.
---
+++ 03_basics/01functions.js
@@ -0,0 +1,24 @@
+// function package of code ek baar create kar do ab use jaha le jana hai uski copy le jao.
+function sayMyName() {
+    console.log("P");
+    console.log("R");
+    console.log("I");
+    console.log("T");
+    console.log("A");
+    console.log("M");  
+}
+//sayMyName // function referrence
+// sayMyName()  // function execute
+
+
+// Add two numbers
+// in java script me number ki types dene ki jarurat nahi hai
+
+function addTwoNumbers(number1 , number2) {
+    console.log(number1 + number1);
+    
+}
+addTwoNumbers(3 , 4)  // output:- 6
+// addTwoNumbers( 3 , "5")  // output:- 35
+
+
