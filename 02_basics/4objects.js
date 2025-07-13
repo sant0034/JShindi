de029603d615440c1f5798d8994d9540f43abc84
sant0034 <87804487+sant0034@users.noreply.github.com>
2025-07-13 06:32:18 +0000

    start function

--- 02_basics/4objects.js
+++ 02_basics/4objects.js
@@ -61,19 +61,43 @@ const users = [
         email: "h@gmail.com"
     }
 ]
-console.log(tinderUser);
-console.log(Object.keys(tinderUser));  // tinderUser Object ke sabhi key ka output and 
+//console.log(tinderUser);
+//console.log(Object.keys(tinderUser));  // tinderUser Object ke sabhi key ka output and 
 // uska data type Array hoga. see output:-  [ 'id', 'name', 'isLoggedIn' ] ab aap chahe to lopp laga sakte hai and aur jo akrna hai.
 // and same syntax for value
 
-console.log(Object.values(tinderUser));  // output :-  [ '123abc', 'Sammy', false ]
+//console.log(Object.values(tinderUser));  // output :-  [ '123abc', 'Sammy', false ]
 
-console.log(Object.entries(tinderUser));  // har key value ko array me convert kar diya jata hai.
+//console.log(Object.entries(tinderUser));  // har key value ko array me convert kar diya jata hai.
 // output:-  [ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
 
 // here we checked the value inside the object
-console.log(tinderUser.hasOwnProperty('isLogged'));  //Determines whether an object has a property with the specified name.
+//console.log(tinderUser.hasOwnProperty('isLogged'));  //Determines whether an object has a property with the specified name.
 // output :- false so always retrun type is boolean.
 
-console.log(users[1].email); // array ke 1 object ki email ki value in output me aayegi.
+//console.log(users[1].email); // array ke 1 object ki email ki value in output me aayegi.
+
+
+
+/////   Dstructure in objects
+const course = {
+    coursename: "java",
+    price: "99",
+    courseInstructor: "Pritam"
+}
+//console.log(course["courseInstructor"]);
+
+const {courseInstructor} = course // agar aap ko lagta hai courseInstructure bada name hai then uska name only instructor karna hai then
+console.log(courseInstructor);
+const {courseInstructor : instructor} = course // output same hoga ese hi kahate hai object ko dstructure karna, {} dekha esa to samajh lo dstructring ho rahi hai
+console.log(instructor);
+
+
+
+
+
+  
+
+
+
 
---
+++ 03_basics/01_functions.js
@@ -0,0 +1,1 @@
+// function package of code ek baar create kar do ab use jaha le jana hai uski copy le jao.
