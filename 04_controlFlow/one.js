341bcec36f24e46c09e1466ed3ad48b55ee8ee8b
sant0034 <87804487+sant0034@users.noreply.github.com>
2025-07-15 07:45:14 +0000

    if flow is completed

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,56 @@
+// ++++++++++++++ if ++++++++++++++++++++++++++
+
+const isUserLoggedIn = true // here I used assign operator
+if (true) {
+    //console.log(" is true");
+    
+}
+
+// operators :- < , > , <= , >= , == we checked here kya wah equal hai kya , !== not equal value or type
+// != , === it also check the type
+
+if ( 2 == "2") {  // yeha par to true ho jayega ki 2 equal hai 2 ke but acctually esa nahi ek number aur ek string.
+    //console.log("Is 2 equlas to 2");
+    
+}
+
+// ---------  === also check data type ------------------
+if (2 === "2"){  // es scope ke andar hi nahi jayege kyu kyu ki 2 number not equal hai string 2 ke.
+    //console.log("is 2 equal to 2");
+    
+}
+
+// -------------- !== 	not equal value or type -------------------------
+if ( 2 !== "2") {
+    //console.log("Is number 2 is not equal or not type to 2");
+    
+}
+//console.log("I always run if condition is true or false");
+
+
+// ++++++ if - else +++++++++++++++++++++++
+const temperature = 41
+if (temperature === 41){
+    //console.log("less then 50");
+    
+}else {
+    //console.log("greater then 50");
+    
+}
+//console.log("I always execute beacuse I am not in scope");
+
+// --- Another program -----------
+
+const score = 200
+if( score > 100) {
+    const power = "fly"
+    console.log(`User power is : ${power}`);
+    
+}
+console.log(`User power is: ${power}`);  // this line of code is not execute because power string variable is not in scope
+
+
+
+
+
+
