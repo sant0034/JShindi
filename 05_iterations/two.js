864371773a7ecb2bdff44e784c59e34231f00d33
sant0034 <87804487+sant0034@users.noreply.github.com>
2025-07-17 04:50:41 +0000

    while and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,34 @@
+
+// +++++++++ while loop +++++++++++++++++++++++++++++++++++++
+let index = 0
+while (index <=10){
+    //console.log(`Value of index is : ${index}`);
+    index++ 
+}
+
+
+// Array in while loop
+let myArray = ["flash" , "superman" , "batman"];
+let arr = 0;
+while (arr < myArray.length){
+    //console.log(`All myArray are : ${myArray[arr]}`);
+    arr++
+}
+
+//+++++++++++++++++++++ do-while loop +++++++++++++++++++++++++++++++++
+// do-while loop run atleast one time . it does not depend on the condition true or false .
+let score = 1
+do {
+    console.log(`Value of score is ${score}`);
+    score++
+}while(score <= 10);
+
+
+
+
+
+
+
+
+
+
