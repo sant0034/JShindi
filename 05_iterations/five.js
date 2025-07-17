8950c329aed0221f8304c269c50866a2f6b0d90c
sant0034 <87804487+sant0034@users.noreply.github.com>
2025-07-17 10:28:30 +0000

    forEach loop

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -19,7 +19,7 @@ cpp
 
 // now work with Arrow function
 coding.forEach((item) => {  //kaha tha na koe bhi paarameter name ho sakta hai but foucus on variable name.
-    console.log(item);
+   // console.log(item);
     
 })
 // output :-
@@ -33,11 +33,42 @@ cpp
 
 
 
+// here we create function and give refernce to for each loop
+/*
+function printMe(item){
+    console.log(item);
+}
+coding.forEach(printMe)
+*/
 
 
 
+// So now we pass different - different paramenter 
+// coding.forEach((item , index , arr) => {
+//     console.log(`so the index value is :- ${item} and the index is :- ${index} and the array is :- ${arr}`);
+// })
 
 
+//Another Example of loop
+// ek array ke andar multiple object
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName : "Java",
+        languageFileName :"java"
+    },
+    {
+        languageName : "Python",
+        languageFileName : "py"
+    }
+]
+myCoding.forEach((item) => {
+    console.log(item.languageName);
+    
+})
 
 
 
