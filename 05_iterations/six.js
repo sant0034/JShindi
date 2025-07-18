39484fb1d465beee09d70525ec7d8f6f3c8ee976
sant0034 <87804487+sant0034@users.noreply.github.com>
2025-07-18 05:10:33 +0000

    filter example

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -40,10 +40,24 @@ myNums.forEach((num) => {
         mNums.push(num)
     }
 })
-console.log(mNums);
+//console.log(mNums);
 
 
 
-
+// some Example
+const books = [
+    {title: 'Book One' , genre: 'Friction' , publish: 1981, edition: 2004},
+    {title: 'Book Two' , genre: 'Non-Friction' , publish: 1992, edition: 2008},
+    {title: 'Book Three' , genre: 'History' , publish: 1999 , edition: 2007},
+    {title: 'Book Four', genre: 'Non-Friction', publish: 1989 , edition: 2010},
+    {title: 'Book Five' , genre: 'Science', publish: 2009, edition: 2014},
+    {title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010},
+    {title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996},
+    {title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016},
+    {title: 'Book Nine', genre: 'Non-Friction', publish: 1981, edition: 1989}
+];
+// user bol raha hai ki above data me History ki book do only
+const userBooks = books.filter( (bk) => bk.genre === "History")
+console.log(userBooks);
 
 
